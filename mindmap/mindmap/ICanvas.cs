429d4822b1c53bdfdd1c692069fc0f931055c37c
lazaroalvin <lazaroalvin@rocketmail.com>
2016-11-01 11:20:51 +0700

    [ADD] Canvas Interface

---
+++ mindmap/mindmap/ICanvas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mindmap
+{
+    interface ICanvas
+    {
+        String Name { get; set; }
+        ITool GetActiveTool();
+        void SetActiveTool(ITool tool);
+        void Repaint();
+        void SetBackgroundColor(Color color);
+
+        void AddDrawingObject(DrawingObject drawingObject);
+        void RemoveDrawingObject(DrawingObject drawingObject);
+
+        DrawingObject GetObjectAt(int x, int y);
+        DrawingObject SelectObjectAt(int x, int y);
+        void DeselectAllObjects();
+    }
+}
