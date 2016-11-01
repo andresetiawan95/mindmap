751ac84969a67555f51e0615059e770b98576ecc
andresetiawan95 <setiawan.andre03@gmail.com>
2016-11-01 08:37:52 +0700

    [ADD] adding IPanel and ITool interfaces

---
+++ mindmap/mindmap/IPanel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mindmap
+{
+    public interface IPanel
+    {
+        void SetColorBackground(Color color);
+        void Repaint();
+        void SetActiveTools(ITool itool);
+    }
+}
