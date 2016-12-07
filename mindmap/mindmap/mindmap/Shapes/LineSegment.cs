60b7807bd1a3cb73e6b39b0cf630ecae85ce000d
Albertbmm <mebungaranmanik@gmail.com>
2016-12-07 12:18:39 +0700

    update line segment
    buat nanti ngisi line setiap nambah child

--- mindmap/mindmap/mindmap/Shapes/LineSegment.cs
+++ mindmap/mindmap/mindmap/Shapes/LineSegment.cs
@@ -135,5 +135,14 @@ namespace mindmap.Shapes
         {
             throw new NotImplementedException();
         }
+        
+        public void AddVertexStartObject(DrawingObject obj)
+        {
+            this.startVertexObject = obj;
+        }
+        public void AddVertexEndObject(DrawingObject obj)
+        {
+            this.endVertexObject = obj;
+        }
     }
 }
