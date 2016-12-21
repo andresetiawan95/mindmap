a93b8e16292ef6aab93acda4dad8875ed324da6a
David Victor G <davidvgiandly@gmail.com>
2016-12-21 13:12:24 +0700

    Add ItextBox

---
+++ mindmap/mindmap/FormLayout/ITextBox.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using mindmap.Shapes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mindmap.FormLayout
+{
+    public interface ITextBox
+    {
+        IPanel getCanvas { set; get; }
+        TextSegment getText { set; get; }
+
+        void SetText(String text);
+        void SetInitText();
+        void UpdateText();
+    }
+}
